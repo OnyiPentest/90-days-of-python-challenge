4641bf521f36a31610e546757569dc4b6aa86751
OnyiPentest <onyinyechinome100@gmail.com>
2025-01-06 03:03:34 -0500

    Day 1: Introduction to print statment and basic Artithmetic

---
+++ Day1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#Day1 Project Write a simple Python script that prints Hello, World! and the results of basic arithmetic
+
+print ("---------------------------------------------------------------")
+
+# A PYTHON SCRIPT TO PRINT HELLO WORLD
+
+print ("Hello world")
+
+# A PYTHON SCRIPT TO PERFORM  BASIC ARITHMETIC
+
+# Addition
+
+a = 30
+b = 10
+
+print (f"{a} + {b} = {a+b}")
+
+# Subtraction
+
+print (f"{a} - {b} = {a-b}")
+
+# Division
+print (f"{a} / {b} = {a/b}")
+
+
+# Multiplication
+print (f"{a} * {b} = {a*b}")
