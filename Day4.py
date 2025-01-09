8f0b357ca1edb278002c643748d30839e01fcb8c
OnyiPentest <onyinyechinome100@gmail.com>
2025-01-09 13:31:53 -0500

    Day 4: For and While Loop

---
+++ Day4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+Day 4: Loops
+- Topics:
+ - Learn about for and while loops.
+ - Iterating through lists and dictionaries.\
+ 
+- Prject:
+
+ - Create a simple "countdown" program that counts down from 10 to 1 using a while loop.
+'''
+
+
+
+print ("\nA SIMPLE PYTHON COUNTDOWN PROGRAM USING WHILE LOOP\n")
+
+# A COUNTDOWN FROM 10 TO 0
+count = 10
+
+while count > 0:
+    print(count)
+    count -= 1 # Count Decreases by 1
+
+print ("Count Down completed")
